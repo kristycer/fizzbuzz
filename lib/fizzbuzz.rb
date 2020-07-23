4e5e21f12bf275ae98337c6e67d85296b8b5d718
kristyna cervenkova <kristyna275@gmail.com>
2020-07-23 16:08:56 +0200

    fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(number)
-  'fizz'
+  return 'FizzBuzz' if number % 15 == 0
+  return 'Fizz' if number % 3 == 0
+  return 'Buzz' if number % 5 == 0
+  return number
+end
+
+(1..20).each do |number|
+  puts fizzbuzz(number)
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,21 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'return "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'return "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return "Buzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
